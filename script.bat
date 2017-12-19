b7d45469b3a0df02562a1024a244826358f3e891
gregorlaan <gregor@github.ee>
2017-12-19 22:08:02 +0200

    add files via upload

---
+++ script.bat
@@ -0,0 +1,25 @@
+@echo off
+
+net session >nul 2>&1
+
+if not %errorLevel% == 0 (
+    echo Failure: Run this script as Administrator
+    pause
+    exit
+)
+
+(echo list disk) | diskpart
+
+SET /P _select_drive= Please enter drive letter: 
+
+(
+    echo select disk %_select_drive%
+    echo clean
+    echo create partition primary
+    echo select partition 1
+    echo format fs=ntfs quick
+    echo active
+    echo exit
+) | diskpart
+
+pause
